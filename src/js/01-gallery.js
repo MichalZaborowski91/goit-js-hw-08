025d13d95562a8e1bb1cd7e4bc9edd1c1420bc7b
MichalZaborowski91 <mkzaborowski@gmail.com>
2023-04-28 00:12:42 +0100

    hw 1/3

---
+++ src/js/01-gallery.js
@@ -0,0 +1,25 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+function createGallery(items) {
+  return items
+    .map(
+      item => `<li><a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a></li>`
+    )
+    .join('');
+}
+gallery.innerHTML = createGallery(galleryItems);
+gallery.style.listStyle = 'none';
+//Vanilla JavaScript Plugin
+const lightbox = new SimpleLightbox('.gallery a', {
+  //Options
+  captionsData: 'alt',
+  captionDelay: 250,
+});
