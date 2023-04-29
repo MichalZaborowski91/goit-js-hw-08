b01b71c29cd252364630035f47753eca75f65cb1
MichalZaborowski91 <mkzaborowski@gmail.com>
2023-04-29 18:11:09 +0100

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+'use strict';
+import Player from '@vimeo/player'; //improt playera
+import throttle from 'lodash.throttle'; //import throttle
 
+const iframe = document.querySelector('#vimeo-player'); //znalezienie <iframe>
+const videoPlayer = new Player(iframe); //inicjacja playera
+const throttledTime = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000); //zapisanie do localStorage zthrottlowanego czasu z przeskokiem co 1000ms do klucza 'videoplayer-current-time'
+videoPlayer.on('timeupdate', throttledTime); //skladnia player.on
+
+const currentTime = localStorage.getItem('videoplayer-current-time'); //pobranie curretnTime z localStorage i przypisanie
+videoPlayer.setCurrentTime(currentTime); //wywolanie na playerze currentTime
