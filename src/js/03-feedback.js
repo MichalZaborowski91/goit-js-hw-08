26afd632ca0632c12e1e2c64d38048bb231ced7a
MichalZaborowski91 <mkzaborowski@gmail.com>
2023-05-02 08:58:23 +0100

    feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+'use strict';
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('[name="email"]');
+const messageInput = document.querySelector('[name="message"]');
+
+form.addEventListener('input', throttle(saveFormInput, 500));
+form.addEventListener('submit', submitForm);
+
+let dataForm = {};
+
+function saveFormInput() {
+  const dataInput = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataInput));
+  dataForm = dataInput;
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  if (!event.target.email.value || !event.target.message.value) {
+    alert('Please enter values: email and message');
+    return;
+  }
+  event.target.reset();
+  console.log(dataForm);
+  localStorage.removeItem('feedback-form-state');
+}
